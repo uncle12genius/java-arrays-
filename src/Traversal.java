9530a352fd1b5f9b161a1d8edcf5eff904859f9a
uncle12genius <omondievan461@gmail.com>
2025-04-09 13:48:29 +0300

    trasvasal array

---
+++ src/Traversal.java
@@ -0,0 +1,14 @@
+public class Traversal {
+    public static void main (String[]args){
+        int [] num = {18, 20 , 15 , 59,  60 };
+
+        System.out.println("The elements are :\n ");
+
+        for (int i = 0 ;i < num.length; i++ ){
+
+            System.out.printf("num [%d] = %d\n ", i , num[i]);
+
+
+        }
+    }
+}
